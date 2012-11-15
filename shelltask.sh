4528e9268a5da3969272ff355565cb3df31bd604
Thomas DE BONA <thomas.debona@gmail.com>
2012-11-16 00:31:54 +0100

    Change the way shelltask_root is resolved

--- shelltask.sh
+++ shelltask.sh
@@ -6,49 +6,55 @@
 # [$2 => task sub-command to call]
 # [$+ => task sub-command options]
 
-export SHELLTASK_ROOT=$(dirname $0)
-export SHELLTASK_LIBS="$SHELLTASK_ROOT/libs"
-export SHELLTASK_TASKS="$SHELLTASK_ROOT/tasks"
+
+SHELLTASK_ROOT="$( cd -P "$( dirname "$0" )" && pwd )"
+
+
+SHELLTASK_LIBS="$SHELLTASK_ROOT/libs"
+SHELLTASK_TASKS="$SHELLTASK_ROOT/tasks"
+
 
 source "$SHELLTASK_LIBS/shelltask_functions.sh"
 
 task_name="$1"
 task_file="$SHELLTASK_TASKS/$task_name.task.sh"
 
-if [[ -r $task_file ]]
+if [[ ! -r $task_file ]]
 then
-	# load task functions
-	source $task_file
-
-	command_name="$2"
-	function_name="${task_name}_${command_name}"
-
-	# if command_name is help
-	if [[ $2 = "help" ]]
-	then
-		# then gerate basi help
-		echo "TODO synopsis $task"
-	fi
-
-	# if function_name can be called (i.e. a function)
-	if type $function_name &> /dev/null
-	then
-
-		# http://tldp.org/LDP/abs/html/internalvariables.html#INCOMPAT
-		# be carefull using $* and $@
-		
-		shift # shift parameter list (remove $1 from parameter list)
-		shift
-
-		# now, parameter list does not include $task_name and $command_name
-		$function_name "$@"
-	else
-		# else print repo help
-		echo "$task_name ${redf}$command_name${reset} does not exist!"
-
-		# Recursive call to help sub-command
-		$0 $task_name help
-	fi
-else
-	echo "${redf} ✗ could not find task: $task_name${reset}"
+	bad "could not find task: ${redf}$task_name"
+	exit 1
 fi
+
+# load task functions
+source $task_file
+
+command_name="$2"
+function_name="${task_name}_${command_name}"
+
+# if command_name is help
+if [[ $2 = "help" ]]
+then
+	# then gerate basic help
+	echo "TODO synopsis $task"
+fi
+
+# if function_name can be called (i.e. a function)
+if type $function_name &> /dev/null
+then
+
+	# http://tldp.org/LDP/abs/html/internalvariables.html#INCOMPAT
+	# be carefull using $* and $@
+	
+	shift # shift parameter list (remove $1 from parameter list)
+	shift
+
+	# now, parameter list does not include $task_name and $command_name
+	$function_name "$@"
+else
+	# else print repo help
+	echo "$task_name ${redf}$command_name${reset} does not exist!"
+
+	# Recursive call to help sub-command
+	$0 $task_name help
+fi
+
