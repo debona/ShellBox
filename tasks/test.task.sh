03619acf77c67b37ab3af71a64122c1864c9da7e
Thomas DE BONA <thomas.debona@gmail.com>
2012-11-17 18:49:35 +0100

    Add real tests in test task file

--- tasks/test.task.sh
+++ tasks/test.task.sh
@@ -1,37 +1,71 @@
 #!/bin/bash
 #
-# test shell task
+# Auto-test task file
+# This task file provide some test functions to test ShellTask libraries
 
 
-# TODO : test auto-completion pattern
-
-## Simply test parameters
-# Be carefull with this task... it does nothing
-#
-#1 [file] first arg
-#2 [] arg
-function test_help() {
-	echo "repo without subcommand runs ${bluef}status${reset}"
-}
-
-
-## Default action
-#
-#1 [file] first arg
-#2 [] arg
+## Default command
+# Default command cannot have parameters!
 function test_() {
-	verbose test_test "$@"
+	if [[ "$1" = "" ]]
+	then
+		success "There are no parameters"
+		return 0
+	else
+		failure "There is at least one parameter"
+		return 1
+	fi
+}
+
+## Display all args on one line
+#
+# @params args to display
+function test_oneline () {
+	step "Print all parameters in one line:"
+	success "$@"
+}
+
+## Print one parameter by line
+#
+# @param printed on the first line
+# ...
+# @param printed on the last line
+function test_multiline() {
+	step "Print one parameter by line"
+	if [[ -z $1 ]]
+	then
+		warning "There is no parameters"
+	else
+		for i in `seq 1 $#`
+		do
+			success "$i => '$1'"
+			shift
+		done
+	fi
 }
 
 
-## Simply test parameters
-# Be carefull with this task... it does nothing
-#
-#1 [file] first arg
-#2 [] arg
-function test_test() {
-	echo "1 => $1"
-	echo "2 => $2"
-	echo "3 => $3"
-	echo "4 => $4"
+# Self-tested function
+# This is a self tested function to assert that analysis can extract
+# Note that this command does not respect any code convention
+# Test to extract the raw documentation of task command
+
+test_self_tested ( ){
+	source "$SHELLTASK_LIBS/analysis.sh"
+	first_line=$(command_raw_doc "${BASH_SOURCE[0]}" "self_tested" | head -n 1)
+	last_line=$(command_raw_doc "${BASH_SOURCE[0]}" "self_tested" | tail -n 1)
+
+	if [[ "$first_line" != "# Self-tested function" ]]
+	then
+		failure "First documentation line not found"
+		return 1
+	fi
+
+	if [[ "$last_line" != "# Test to extract the raw documentation of task command" ]]
+	then
+		failure "Last documentation line not found"
+		return 1
+	fi
+
+	success "Documentation found"
 }
