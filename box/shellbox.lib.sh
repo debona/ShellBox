2774939e5e7be7dc060d68ec859a51b4a348b687
Thomas DE BONA <thomas.debona@gmail.com>
2013-04-04 00:43:07 +0200

    Remove all shortcut stuff

--- box/shellbox.lib.sh
+++ box/shellbox.lib.sh
@@ -72,17 +72,6 @@ function shellbox::status() {
 	fi
 
 	shellbox::libraries
-
-	cli::step "Library shortcuts"
-	for lib_name in `list_library_names`
-	do
-		if [[ -x "$SHELLBOX_ROOT/bin/$lib_name" ]]
-		then
-			echo "   - ${purplef}${boldon}$lib_name${greenf} shortcuted${reset}"
-		else
-			echo "   - ${purplef}${boldon}$lib_name${redf} NOT shortcuted${reset}"
-		fi
-	done
 }
 
 
@@ -98,43 +87,6 @@ function shellbox::libraries() {
 		local lib_file=$( locate_library_file "${lib_name}" )
 		local short=$( cat "$lib_file" | analyse::file_raw_doc | sed -E "s/^#[# 	]*(.*)$/\1/g" | head -n 1 )
 		echo "   - ${purplef}${boldon}$lib_name${reset} - $short"
-	done
-}
-
-
-## Create a shortcut for the given libraries
-# A shortcut allow you to run directly a library without prefix it with `shellbox my_library ...`
-# It simply create a symlink in the `bin` folder which target the `main.sh` library executer.
-#
-# @params	lib_names	The names of the libraries to shortcut. 'all' create a shortcut for all available libraries.
-function shellbox::shortcut() {
-	local library="$1"
-
-	if [[ "$library" = "all" ]]
-	then
-		for lib_name in `list_library_names`
-		do
-			shortcut $lib_name
-		done
-	else
-		for lib_name in $@
-		do
-			shortcut $lib_name
-		done
-	fi
-}
-
-
-## Delete a shortcut for the given libraries
-# It simply delete the symlink in the `bin` folder which match the given library name.
-#
-# @params	lib_names	The names of the libraries to unshortcut.
-function shellbox::unshortcut() {
-	local library="$1"
-
-	for lib_name in $@
-	do
-		unshortcut $lib_name
 	done
 }
 
@@ -180,60 +132,4 @@ function extract_todos() {
 		echo "${greenf}`basename $file`${reset}"
 		echo "$todos"
 	done
-}
-
-
-## Create a shortcut for the given library.
-#
-# @param	lib_name	The name of the library to shortcut
-function shortcut() {
-	require 'cli'
-
-	local lib_name="$1"
-	local lib_file=$( locate_library_file "$lib_name" )
-
-	if ! [[ -r "$lib_file" ]]
-	then
-		echo "${redb}${boldon} ✗ could not read $1:${reset}"
-		return 1
-	fi
-
-	ln -s "../main.sh" "$SHELLBOX_ROOT/bin/$lib_name" &> /dev/null
-
-	if [[ -x "$SHELLBOX_ROOT/bin/$lib_name" ]]
-	then
-		cli::step "${boldon}${bluef}$lib_name${reset} available"
-		return 0
-	else
-		cli::failure "couldn't shortcut $lib_name${reset}"
-		return 1
-	fi
-}
-
-## Delete a shortcut for the given library.
-#
-# @param	lib_name	The name of the library to unshortcut
-function unshortcut() {
-	require 'cli'
-
-	local lib_name="$1"
-	local shortcut="$SHELLBOX_ROOT/bin/$lib_name"
-
-	if [[ "$lib_name" = 'shellbox' ]]
-	then
-		cli::failure "Couldn't unshortcut $lib_name"
-		echo "   To delete this shortcut manually, run the following command:"
-		echo " > rm \"$shortcut\""
-		return 1
-	fi
-
-	rm -f "$shortcut" &> /dev/null
-
-	if [[ -e $shortcut ]]
-	then
-		cli::failure "Couldn't unshortcut $lib_name:"
-		rm -f "$shortcut"
-	else
-		cli::step "${boldon}${bluef}$lib_name${reset} no more available"
-	fi
 }
